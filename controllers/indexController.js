345e3f33a745055e2c0d94117deaf6b86c7620a6
nickstuffz <112445360+nickstuffz@users.noreply.github.com>
2024-12-31 15:36:06 -0800

    Finish DB query for donuts details

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,7 +1,8 @@
 const db = require("../db/queries");
 
 async function donutsGet(req, res) {
-  const donuts = await db.getAllDonuts();
+  const donuts = await db.getAllDonutsDetails();
+  console.log(donuts);
   res.render("index", { donuts: donuts });
 }
 
--- db/queries.js
+++ db/queries.js
@@ -1,8 +1,40 @@
 const pool = require("./pool");
 
-async function getAllDonuts() {
-  const { rows } = await pool.query("SELECT * FROM donuts");
+const donutDetailsSQL = `
+SELECT
+  donuts.name AS name,
+  donuts.quantity as quantity,
+  donuts.description as description,
+  types.name AS type,
+  shapes.name AS shape,
+  fillings.name AS filling,
+  ARRAY_AGG(toppings.name) FILTER (WHERE toppings.name IS NOT NULL) AS toppings,
+  (types.price + shapes.price + COALESCE(fillings.price, 0) + COALESCE(SUM(toppings.price), 0)) AS price
+FROM
+  donuts
+  LEFT JOIN types ON donuts.type_id = types.id
+  LEFT JOIN shapes ON donuts.shape_id = shapes.id
+  LEFT JOIN fillings ON donuts.filling_id = fillings.id
+  LEFT JOIN donut_toppings ON donuts.id = donut_toppings.donut_id
+  LEFT JOIN toppings ON donut_toppings.topping_id = toppings.id
+GROUP BY
+  donuts.id,
+  donuts.name,
+  donuts.quantity,
+  donuts.description,
+  types.name,
+  types.price,
+  shapes.name,
+  shapes.price,
+  fillings.name,
+  fillings.price
+ORDER BY
+  donuts.id;
+`;
+
+async function getAllDonutsDetails() {
+  const { rows } = await pool.query(donutDetailsSQL);
   return rows;
 }
 
-module.exports = { getAllDonuts };
+module.exports = { getAllDonutsDetails };
