95005fac0fa160bcb186e14d7b2d448b9ef858d8
nickstuffz <112445360+nickstuffz@users.noreply.github.com>
2024-12-31 22:09:20 -0800

    add styles, handle null values in ejs

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const app = express();
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-
 app.use(express.urlencoded({ extended: true }));
+app.use("/public", express.static("public"));
 
 app.use("/", indexRouter);
 
